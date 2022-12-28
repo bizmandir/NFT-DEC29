8f4881a072d9df670b6e3a080d3e08fa6893f858
bizmandir <119396680+bizmandir@users.noreply.github.com>
2022-12-28 11:53:35 +0530

    Add files via upload

---
+++ Context/constants.js
@@ -0,0 +1,5 @@
+import nftMarketplace from "./NFTMarketplace.json";
+
+export const NFTMarketplaceAddress =
+  "0xc3023a2c9f7b92d1dd19f488af6ee107a78df9db";
+export const NFTMarketplaceABI = nftMarketplace.abi;
